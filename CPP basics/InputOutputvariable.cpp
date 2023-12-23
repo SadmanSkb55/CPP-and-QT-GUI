1247d97fa2b4fbac5f9fe87e30c798a5fdf5acea
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2023-12-23 23:43:42 +0600

    Input Output And Variable

---
+++ CPP basics/InputOutputvariable.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int age;
+ string name;
+ float code;
+ double id;
+ bool verif;
+ char firstletter;
+ 
+
+    cout << "Enter name" <<endl;
+    cin>>name;
+    cout<<"Enter first letter of name";
+    cin>>firstletter;
+    cout<<"Enter Age";
+    cin>>age;
+    cout<<"Enter code";
+    cin>>code;
+    cout<<"Enter ID number";
+    cin>>id;
+    cout<<"enter true of false:Are you dumb?";
+    cin>>verif;
+
+        cout<<"Name is: "<<name<<endl;
+        cout<<"First Latter is: "<<firstletter<<endl;
+        cout<<"Code is: "<<code<<endl;
+        cout<<"ID is: "<<id<<endl;
+        cout<<"Age is: "<<age<<endl;
+        cout<<"\n";
+        cout<<"Thank you";
+    return 0;
+}
