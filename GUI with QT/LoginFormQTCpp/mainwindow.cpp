0ce56a5397f5e60bb48e40ae5c2b21f75674a0e2
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-01-15 01:10:03 +0600

    Status bar and loading

--- GUI with QT/LoginFormQTCpp/mainwindow.cpp
+++ GUI with QT/LoginFormQTCpp/mainwindow.cpp
@@ -3,17 +3,25 @@
 #include<QMessageBox>
 #include<QPixmap>
 #include<QMovie>
+#include<QTimer>
+#include<QThread>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->statusbar->addPermanentWidget(ui->label_3,1);
+    ui->statusbar->addPermanentWidget(ui->progressBar,1);
+
+    //this code is for image:::::
    //  QPixmap pix("D:\\QTGUICPP\\QTCPPProjects\\LoginFormQTCpp\\toothless.jpg");
    //  int w=ui->label_pic->width();
    // int h=ui->label_pic->height();
    //  ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 
+    //for gif,QMovie
    QMovie* movie = new QMovie("D:\\QTGUICPP\\QTCPPProjects\\LoginFormQTCpp\\toothlessgif.gif");
    ui->label_pic->setMovie(movie);
    // Start the GIF animation
@@ -35,13 +43,33 @@ void MainWindow::on_pushButton_clicked()
     QString pass=ui->lineEdit_2->text();
 
     if(user=="Admin" && pass=="Admin"){
+        ui->statusbar->showMessage("Verification Complete",5000);
+        ui->label_3->setText("Yatta!!!");
+         simulateLoading();
         QMessageBox::information(this,"Welcome","User Authenticated");
         hide();
         afterlogin=new AfterLogin(this);
         afterlogin->show();
         afterlogin->exec();
 
-    }else
+    }else{
+         ui->statusbar->showMessage("Verification Failed",5000);
+        ui->label_3->setText("Snapped!!!");
         QMessageBox::warning(this,"False Information","Enter Correct username and password");
+    }
+}
+void MainWindow::simulateLoading()
+{
+    int duration = 5000; // milliseconds
+    int interval = 50;   // milliseconds
+    int steps = duration / interval;
+    int progress = 0;
+    for (int i = 0; i <= steps; ++i) {
+        ui->progressBar->setValue(progress);
+        progress += 100 / steps;
+        QCoreApplication::processEvents();
+        QThread::sleep(interval / 1000); // Sleep for 'interval' seconds
+    }
+    ui->progressBar->setValue(100);
 }
 
