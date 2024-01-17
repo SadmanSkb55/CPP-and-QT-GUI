491c46a0b17403ebf95e638f54b2cc5c097edcb8
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-01-17 18:09:28 +0600

    Q File Dialogue

---
+++ GUI with QT/QFileDialogue/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QFileDialog>
+#include<QMessageBox>
+#include<QDir>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString fileName=QFileDialog::getOpenFileName(this,"Open A File",QDir::homePath());
+    QMessageBox::information(this," ",fileName);
+}
+
