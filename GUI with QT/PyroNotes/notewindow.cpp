5b0ee99359ad312a64bc66491af56447f1c2a567
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-01-24 16:05:12 +0600

    just functions

--- GUI with QT/PyroNotes/notewindow.cpp
+++ GUI with QT/PyroNotes/notewindow.cpp
@@ -28,3 +28,93 @@ void NoteWindow::on_actionNew_Tab_triggered()
     ui->tabWidget->setCurrentIndex(ui->tabWidget->count()-1);
 }
 
+
+void NoteWindow::on_actionNew_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionSave_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionSave_As_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionPrint_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionCut_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionCopy_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionPaste_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionUndo_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionRedo_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionFont_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionColor_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionHighlighter_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionBackground_Color_triggered()
+{
+
+}
+
+
+void NoteWindow::on_actionAbout_PyroNote_triggered()
+{
+
+}
+
