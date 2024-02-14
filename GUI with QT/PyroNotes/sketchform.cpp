2d9bb9e1799509cc097b8c9d2a316ef763bc5c9a
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-02-14 17:22:27 +0600

    sketchform success

--- GUI with QT/PyroNotes/sketchform.cpp
+++ GUI with QT/PyroNotes/sketchform.cpp
@@ -2,24 +2,37 @@
 #include "ui_sketchform.h"
 #include "sketchform.h"
 #include "sketchfcompanion.h"
+#include <QInputDialog>
+#include <QColorDialog>
+#include <QColor>
+
 
 SketchForm::SketchForm(NoteWindow *parent)
     : QMainWindow(parent)
     , ui(new Ui::SketchForm)
 {
     ui->setupUi(this);
-    sketchFCompanion = new SketchFCompanion;
+
+    sketchFCompanion = new SketchFCompanion(this);
     setCentralWidget(sketchFCompanion);
-    resize(500, 500);
+   // resize(500, 500);
 }
 
-
+SketchForm::~SketchForm()
+{
+    delete ui;
+}
 
 void SketchForm::on_actionPen_Color_triggered()
 {
-    QColor newColor = QColorDialog::getColor(sketchFCompanion->penColor());
-    if (newColor.isValid())
-        sketchFCompanion->setPenColor(newColor);
+   // bool ok;
+    QColor color = QColorDialog::getColor(Qt::white, this, "Choose Color");
+    //if(ok){
+    if (color.isValid())
+        sketchFCompanion->setPenColor(color);
+    else
+        sketchFCompanion->setPenColor(Qt::blue);
+   // }
 }
 
 void SketchForm::on_actionPen_Width_triggered()
@@ -37,3 +50,28 @@ void SketchForm::on_actionClear_triggered()
 {
     sketchFCompanion->clearImage();
 }
+
+void SketchForm::on_actionEraser_triggered()
+{
+    // Ask the user for the size of the eraser
+    bool ok;
+    int eraserSize = QInputDialog::getInt(this, tr("Eraser Size"),
+                                          tr("Select eraser size:"),
+                                          sketchFCompanion->penWidth(),
+                                          1, 50, 1, &ok);
+    if (!ok)
+        return; // User canceled
+
+    // Set the pen color to white (or background color) for erasing
+    sketchFCompanion->setPenColor(Qt::white); // Assuming you want to erase to white
+
+    // Set the pen width to the selected eraser size
+    sketchFCompanion->setPenWidth(eraserSize);
+}
+
+
+void SketchForm::on_actionPen_triggered()
+{
+    sketchFCompanion->setPenColor(Qt::black);
+}
+
--- GUI with QT/PyroNotes/sketchform.h
+++ GUI with QT/PyroNotes/sketchform.h
@@ -1,6 +1,7 @@
 #ifndef SKETCHFORM_H
 #define SKETCHFORM_H
 
+#include "ui_sketchform.h"
 #include <QList>
 #include <QMainWindow>
 #include "notewindow.h"
@@ -22,6 +23,10 @@ private slots:
     void on_actionPen_Width_triggered();
 
     void on_actionClear_triggered();
+    void on_actionEraser_triggered();
+
+    void on_actionPen_triggered();
+
 private:
     Ui::SketchForm *ui;
     SketchFCompanion *sketchFCompanion;
