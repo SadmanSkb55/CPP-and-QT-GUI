45091d42993690e878277bdf9959e035ee0a32e0
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-02-19 23:13:30 +0600

    form

--- GUI with QT/SQLite_DBQTCpp/mainwindow.cpp
+++ GUI with QT/SQLite_DBQTCpp/mainwindow.cpp
@@ -6,16 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLDB");
-    mydb.setDatabaseName("D:\SQLite\database.db");
+    mydb = QSqlDatabase::addDatabase("QSQLITE"); // Corrected driver name
+    mydb.setDatabaseName("D:/SQLite/database.db"); // Corrected path using forward slashes or escaped backslashes
     if(!mydb.open())
-        ui->label->setText("Failed to connect DataBase");
+        ui->label->setText("Failed to connect to the database");
     else
-     ui->label->setText("connected to the database");
-
+        ui->label->setText("Connected to the database");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString pass,uname;
+    uname=ui->lineEdit->text();
+    pass=ui->lineEdit_2->text();
+    if(!mydb.isOpen()){
+                ui->label->setText("Failed to connect to the database");
+        return;
+    }
+
+}
+
--- GUI with QT/SQLite_DBQTCpp/mainwindow.h
+++ GUI with QT/SQLite_DBQTCpp/mainwindow.h
@@ -20,7 +20,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSqlDatabase mydb ;
 };
 #endif // MAINWINDOW_H
